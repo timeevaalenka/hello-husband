8f5a1083158d8359a83f93c74b53c42a82a0342f
alenkatimeeva <138889813+timeevaalenka@users.noreply.github.com>
2023-07-07 21:39:11 +0500

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+     output.innerHTML = 'Мужчина из моей мечты Однажды стал мне верным мужем';
+});
